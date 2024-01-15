b2d65d0a81a8ce3ef96999026a9b74d548085b8e
Rahul.Ramjit <Rahul.Ramjit@MLBPRCL-110148.in.marlabs.com>
2024-01-15 16:18:46 +0530

    First Commit

---
+++ BootMVCProj01-DispalyHomePage1/src/main/java/com/example/demo/BootMvcProj01DispalyHomePage1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj01DispalyHomePage1Application {
+
+	
+	  public static void main(String[] args) {
+		  SpringApplication.run(BootMvcProj01DispalyHomePage1Application.class, args);
+	  }
+	 
+
+}
